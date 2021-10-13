681d7809131b7a50bd60c6dabcc956a9e8f2f172
Kieran <kemackle98@gmail.com>
2021-10-13 13:22:05 +1000

    docs: verified

--- build_LTM.m
+++ build_LTM.m
@@ -1,4 +1,9 @@
 function LTM = build_LTM(m,Hp)
 % Constructs lower triangular matrix of identity matrices of size m x m.
 
-LTM = 1;
+I = eye(m);
+LTM = zeros(m*Hp, m*Hp);
+
+for i = 1:Hp
+    LTM((i-1)*m+1:i*m, 1:m*i) = repmat(I,1,i);
+end
